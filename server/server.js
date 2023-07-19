42ffde152cd3b73c3f921e1cc4a9d11138fda20d
Hasnain <mrajhasnain101@gmail.com>
2023-07-19 22:35:06 +0600

    create

--- server/server.js
+++ server/server.js
@@ -1,18 +1,43 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+// const Tank = mongoose.model('Tank', yourSchema);
 const app = express();
 app.use(cors());
-mongoose.connect(process.env.db);
 
-const Port= process.env.port || 8000
+const Port = process.env.port || 8000;
+const url = process.env.db
+const dbName = process.env.dbName
 
-app.get('/',(req,res)=>{
-    res.send("Hello Hasnain is sending data to port 8000");
-})
-app.listen(8000, () => {
-  console.log(`Server started on port : http://localhost:${Port}`);
+mongoose.connect(url,{
+    dbName: dbName,
+}).then(() => {
+    console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
 });
 
 
+app.get("/getUsers", async (req, res) => {
+  let result = await UserModel.find({});
+  console.log(result);
+  return res.status(200).json(result);
+});
+
+app.get("/createuser", async (req, res) => {
+    let result = await UserModel.create({
+        username: "test",
+        age: 24,
+        name: "test name"
+    });
+    console.log(result);
+    return res.status(200).json(result);
+  });
+
+
+
+app.listen(8000, () => {
+  console.log(`Server started on port : http://localhost:${Port}`);
+});
