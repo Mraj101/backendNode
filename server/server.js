2233b09dc04ff7e39a7fb75b15d01a652a5b2466
Hasnain <mrajhasnain101@gmail.com>
2023-07-19 17:18:50 +0600

    ignore some files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const mongoose=require("mongoose");
+const app = express();
 app.use(cors());
 mongoose.connect(process.env.db);
 
+const Port= process.env.port || 8000
 
-
+app.get('/',(req,res)=>{
+    res.send("Hello Hasnain is sending data to port 8000");
+})
 app.listen(8000, () => {
-  console.log("Server started on port 8000");
+  console.log(`Server started on port : http://localhost:${Port}`);
 });
+
+
