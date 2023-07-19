5090b86531ecc833a46d879b126a44d4eb819c4f
Hasnain <mrajhasnain101@gmail.com>
2023-07-19 14:56:10 +0600

    set proxy port and data fetch

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
 const express=require("express");
 const app=express();
 
+app.get("/api",(req,res)=>{
+    res.json({
+        "users":["userOne","userTwo","userThree"]
+    })
+})
+
+app.listen(8000,()=>{
+    console.log("Server started on port 8000");
+})
