646d295a33b7b48ffe435b6e7ce31f10f9dffcb4
Hasnain <mrajhasnain101@gmail.com>
2023-07-20 18:22:09 +0600

    successfully posted data in database using postman dummy data

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -2,10 +2,13 @@ const express=require('express');
 
 const router=express.Router();
 
+
 // const workController = require('../controller/workController')
 
 
 //get all workouts
+
+const workoutModels = require('../models/workoutModels');
 
 router.get("/", async (req, res) => {
     res.json({mssg:'get all workouts'})
@@ -16,6 +19,13 @@ router.get("/", async (req, res) => {
   });
   
   router.post("/",async (req, res) => {
+    try {
+        const {title,reps,load}=req.body;
+        let data= await workoutModels.create({title,reps,load})
+        return res.status(201).json(data)
+    } catch (error) {
+        res.status(400).json({error:error.message})
+    }
     res.json({mssg:'post workouts'})
   });
   router.delete("/:id", async (req, res) => {
