3b727bbeb48fa072e801b17c8d8ee524e24ad02b
Hasnain <mrajhasnain101@gmail.com>
2023-07-21 23:21:15 +0600

    new commit client

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [backendData, setBackEndData] = useState([]);
+
+  
+
+
+ 
+  return (
+    <div>
+     <p>hI</p>
+    </div>
+  );
+};
+
+export default App;
