1374707b227f93a7b5b614cee48b69cd0f95f8da
Hasnain <mrajhasnain101@gmail.com>
2023-07-24 17:13:09 +0600

    new user added in routes

---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+const { loginUser,signupUser   }=require('../controllers/userController')
+
+const express=require('express');
+const router=express.Router()
+
+//login route
+
+router.post('/login',loginUser)
+
+//signup route
+router.post('/login',signupUser)
