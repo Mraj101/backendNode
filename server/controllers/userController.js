55dd464853b5c27278583a7f022218a02cb016b5
Hasnain <mrajhasnain101@gmail.com>
2023-07-26 16:45:20 +0600

    posted a user signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,10 +12,10 @@ const signupUser=async(req,res)=>{
     const {email,password}=req.body
     
     try{
-        const user=await user.singup(email,password)
+        const user=await User.signup(email,password)
         res.status(200).json({email,user})
     }catch(error){
-        res.status(404).json({error:error.messge})
+        res.status(400).json({error:error.messge})
     }
 
     
