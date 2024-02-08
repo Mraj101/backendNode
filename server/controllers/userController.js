fab2f865b8cff4590c12b7cd2b3cc8f11660be29
Hasnain <mrajhasnain101@gmail.com>
2024-02-08 18:37:02 +0600

    user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,43 +1,39 @@
-const User=require('../models/userModel')
-const jwt=require('jsonwebtoken');
-require('dotenv').config();
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const createToken=(_id)=>{
-    return jwt.sign({_id:_id},process.env.secret,{expiresIn:'1h'});
-}
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.secret, { expiresIn: "1h" });
+};
+
 //login  user creation
-const loginUser=async(req,res)=>{
-   const {email,password}=req.body;
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-   try{
-    const user=await User.login(email,password);
-    const token=createToken(user._id);
-    res.status(200).json({email,token});
-}
-catch(error){
-    res.status(400).json({error:error.message})
-}
-
-}
-
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: "error for the login" });
+  }
+};
 
 //signup User creation
-const signupUser = async(req,res)=>{
-    const {email,password}=req.body
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
 
-    try{
-        const user=await User.signup(email,password);
-        const token=createToken(user._id);
-        
-        res.status(200).json({email,token});
-    }
-    catch(error){
-        res.status(400).json({error:error.message})
-    }
-}
+  try {
+    const user = await User.signup(email, password);
+    const token = createToken(user._id);
 
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-module.exports={
-    loginUser,
-    signupUser
-}
+module.exports = {
+  loginUser,
+  signupUser,
+};
