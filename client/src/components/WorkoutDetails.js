0c229f0cd4f7031d03f3aa35a54f1798618d0a22
Hasnain <mrajhasnain101@gmail.com>
2024-02-09 23:56:58 +0600

    workouts

--- client/src/components/WorkoutDetails.js
+++ client/src/components/WorkoutDetails.js
@@ -1,11 +1,18 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useAuthContext } from "../hooks/useAuthContext";
 const WorkoutDetails = ({ workout }) => {
-
+    const {user}=useAuthContext();
     const {dispatch}=useWorkoutsContext();
     const handleClick =async()=>{
+      if(!user){
+        return
+      }
       const response=await fetch('/api/workouts/' + workout._id,{
-        method:'DELETE'
+        method:'DELETE',
+        headers:{
+          'Authorization':`Bearer ${user.token}`
+        }
       });
       const json=await response.json()
 
